75d5ae98a7f7442fc1a7fe71296a0fd2bfb6b698
zunda <zunda at freeshell.org>
2011-10-03 21:03:21 -1000

    Created a default Android project with Eclipse

---
+++ src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetsActivity.java
@@ -0,0 +1,13 @@
+package org.freeshell.zunda.android.SprintMultipleWidgets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SprintMultipleWidgetsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
