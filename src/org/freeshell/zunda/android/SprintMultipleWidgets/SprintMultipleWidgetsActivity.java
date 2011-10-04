ef2d31ea3009cdf9b9d97a7b476ea2e83a794d5c
zunda <zunda at freeshell.org>
2011-10-03 21:35:47 -1000

    Modified to create a Service that shows a Toast when started

--- src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetsActivity.java
+++ src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetsActivity.java
@@ -1,13 +1,39 @@
 package org.freeshell.zunda.android.SprintMultipleWidgets;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SprintMultipleWidgetsActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private final String LogTag = "SprintMultipleWidgetsActivity";
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Log.v(LogTag, "onCreate()");
+		setContentView(R.layout.main);
+
+		((Button) findViewById(R.id.toastButton))
+				.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						startSprintMultipleWidgetsService();
+					}
+				});
+	}
+	
+	private void startSprintMultipleWidgetsService() {
+		Log.v(LogTag, "startSprintMultipleWidgetsService()");
+		Intent intent = new Intent();
+		intent.setClass(this, SprintMultipleWidgetsService.class);
+		intent.putExtra("MSG", "Hello, World!");		
+		startService(intent);
+	}
 }
---
+++ src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetsService.java
@@ -0,0 +1,34 @@
+package org.freeshell.zunda.android.SprintMultipleWidgets;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SprintMultipleWidgetsService extends Service {
+	private final String LogTag = "SprintMultipleWidgetsService";
+
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		Log.v(LogTag, "onStart()");
+
+		if (intent == null) {
+			stopSelf();
+			return;
+		}
+
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			Toast.makeText(getApplicationContext(), extras.getString("MSG"), Toast.LENGTH_SHORT).show();
+		}
+	}
+}
