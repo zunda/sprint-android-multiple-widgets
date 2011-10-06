0dec404391d75d64bde0de056851539ffe970d1d
zunda <zunda at freeshell.org>
2011-10-05 21:52:00 -1000

    Finally got a widget on the home screen

--- src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetAppWidgetProvider.java
+++ src/org/freeshell/zunda/android/SprintMultipleWidgets/SprintMultipleWidgetAppWidgetProvider.java
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 public class SprintMultipleWidgetAppWidgetProvider extends AppWidgetProvider {
 	private final String LogTag = "SprintMultipleWidgetAppWidgetProvider";
@@ -11,6 +12,12 @@ public class SprintMultipleWidgetAppWidgetProvider extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		Log.v(LogTag, "onUpdate()");
+
+		RemoteViews widget = new RemoteViews(context.getPackageName(),
+				R.layout.appwidget);
+		widget.setTextViewText(R.id.textView, "Hello");
+
+		appWidgetManager.updateAppWidget(appWidgetIds[0], widget);
 
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 	}
